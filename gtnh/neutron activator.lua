0f72a7f3ba5fd2f62b9fda0579a4bf574c39a4a8
Divran <arviddivran@gmail.com>
2022-07-23 17:30:57 +0200

    Update neutron activator.lua

--- gtnh/neutron activator.lua
+++ gtnh/neutron activator.lua
@@ -6,6 +6,10 @@ local sides = require("sides")
 local redstone = component.redstone
 local neutron = component.gt_machine
 
+local MeV = 215e6 -- Naq-Ad Solution => Adamantite, Naquadah Oxide, Naquadah-rich solution
+--local MeV = 1075e6 -- Naquadah-rich solution => naquadria sulphate
+--local MeV = 470e6 -- Concentrated enriched-naquadah sludge => enriched-naquadah sulphate, sodium sulfate, low quality naquadria
+
 function asdf()
   while true do
     term.clear()
@@ -17,7 +21,7 @@ function asdf()
     info = string.gsub(info, ",", "")
     info = info:sub(33)
     info = tonumber(info)
-    if(info < 1075e6) then
+    if(info < MeV) then
       print("Sending redstone")
       redstone.setOutput(sides.bottom, 15)
     else
