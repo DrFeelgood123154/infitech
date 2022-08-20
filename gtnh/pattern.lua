3e4d595de14d0602fffdf48cba42440fb743e1df
Divran <arviddivran@gmail.com>
2022-08-21 01:59:23 +0200

    Update pattern.lua

--- gtnh/pattern.lua
+++ gtnh/pattern.lua
@@ -1,3 +1,79 @@
+local component = require("component")
+local term = require("term")
+local me = component.me_interface
+local db = component.database
+
+local DBSIZE = 25 -- tier 2 database
+-- local DBSIZE = 81 -- tier 3 database
+
+local function readio(allowboth)
+	while true do
+		local iostr = io.read()
+		if iostr == "i" or iostr == "in" then
+			return "Input"
+		elseif iostr == "o" or iostr == "out" then
+			return "Output"
+		elseif allowboth and (iostr == "b" or iostr == "both") then
+			return "both"
+		else
+			io.write("Invalid, expected 'i' or 'o' " .. (allowboth and "or 'b'" or "") .. ", try again ")
+		end
+	end
+end
+
+while true do
+	local slots = {Input=0, Output=0}
+
+	io.write("Change only input or only output or both? [i/o/b] ")
+	local ioboth = readio(true)
+
+	for i=1,DBSIZE do
+		local dbitem = db.get(i)
+		if dbitem == nil then break end
+
+		print("\nItem: " .. (dbitem.fluid_label and (dbitem.fluid_label .. " Cell") or dbitem.label))
+		local funcName
+
+		if ioboth == "both" then
+			io.write("Is this item an input or output? [i/o] ")
+			funcName = readio(false)
+		else
+			funcName = ioboth
+		end
+
+		io.write("How many of this item? [e to exit] ")
+		local amount
+		repeat
+			local temp = io.read()
+			if temp == "e" then break end
+			amount = tonumber(temp)
+			if amount == nil then io.write("Not a valid number, try again ") end
+		until amount ~= nil
+
+		if not amount then break end
+
+		local maxAmount = amount
+		repeat
+			local currentAmount = math.min(amount,64)
+			amount = amount - currentAmount
+
+			slots[funcName] = slots[funcName] + 1
+			io.write(string.format("Writing %s (%s/%s) items to %s slot %s",
+				currentAmount, maxAmount-amount, maxAmount,
+				string.lower(funcName), slots[funcName]))
+
+			me["setInterfacePattern"..funcName](1, db.address, i, currentAmount, slots[funcName])
+			if amount > 0 then term.clearLine() else io.write("\n") end
+		until (amount == 0)
+	end
+
+	print("DONE! Press enter to go again")
+	io.read()
+end
+
+--[[
+-- dr's version below
+
 component = require("component")
 me = component.me_interface
 db = component.database
@@ -49,3 +125,4 @@ for inp=1,nTypes do
 	end
 	lastSlot = lastSlot + cycles
 end
+]]
